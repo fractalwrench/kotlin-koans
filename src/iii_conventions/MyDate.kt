301b82050ca239eec233dd1f091ad40b3786c923
fractalwrench <fractalwrench@gmail.com>
2018-05-30 17:28:51 +0100

    pass 28 for loop

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -8,8 +8,12 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
     operator fun contains(d: MyDate): Boolean {
         return d >= start && d <= endInclusive
+    }
+
+    override operator fun iterator(): Iterator<MyDate> {
+        return DateRangeIterator(this)
     }
 }
